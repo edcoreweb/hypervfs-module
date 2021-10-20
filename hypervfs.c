6824e500ed98be7bc6070b62fecf9e0fa5431621
Eduard Tofan <edcoreweb@gmail.com>
2021-10-21 01:15:31 +0300

    Mount wip

--- hypervfs.c
+++ hypervfs.c
@@ -7,10 +7,37 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Eduard Tofan");
 MODULE_ALIAS_FS("hypervfs");
 
+static int hypervfs_fill_super(struct super_block *sb, void *data, int silent)
+{
+	struct inode *inode = new_inode(sb);
+	if (inode) {
+		inode->i_ino = get_next_ino();
+		inode->i_atime = inode->i_mtime = inode->i_ctime = current_time(inode);
+		inode->i_mode = S_IFDIR | 0750;
+		inode->i_op = &simple_dir_inode_operations;
+		inode->i_fop = &simple_dir_operations;
+		inc_nlink(inode);
+	}
+
+
+	sb->s_root = d_make_root(inode);
+	if (!sb->s_root)
+		return -ENOMEM;
+
+	return 0;
+}
+
+static struct dentry *hypervfs_mount(struct file_system_type *fs_type,
+	int flags, const char *dev_name, void *data)
+{
+	return mount_single(fs_type, flags, data, hypervfs_fill_super);
+}
+
 static struct file_system_type hypervfs_fs_type = {
 	.owner			= THIS_MODULE,
 	.name			= "hypervfs",
-	.kill_sb		= kill_anon_super
+	.kill_sb		= kill_anon_super,
+	.mount			= hypervfs_mount
 };
 
 static int __init hypervfs_init(void)
