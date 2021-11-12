1444efa8d7f65e414626ef5cf2c69a4783f05c81
Eduard Tofan <edcoreweb@gmail.com>
2021-11-12 22:20:41 +0200

    Ops..

--- inode.c
+++ inode.c
@@ -106,25 +106,130 @@ void hypervfs_evict_inode(struct inode *inode)
 	filemap_fdatawrite(&inode->i_data);
 }
 
+static int hypervfs_mknod(struct inode *dir, struct dentry *dentry, umode_t omode, dev_t rdev)
+{
+	int err;
+	const unsigned char *name;
+	umode_t mode;
+	struct inode *inode;
+	// struct posix_acl *dacl = NULL, *pacl = NULL;
+
+	// err = hypervfs_acl_mode(dir, &mode, &dacl, &pacl);
+	// if (err) {
+	// 	goto error;
+	// }
+
+	name = dentry->d_name.name;
+
+	// err = hypervfs_op_mknod(name, mode);
+	if (err)
+		goto error;
+
+	// hypervfs_invalidate_inode_attr(dir);
+
+	// inode = hypervfs_get_new_inode(ino);
+	// if (IS_ERR(inode)) {
+	// 	err = PTR_ERR(inode);
+	// 	goto error;
+	// }
+
+	// hypervfs_set_create_acl(inode, dacl, pacl);
+	// d_instantiate(dentry, inode);
+	err = 0;
+error:
+	// posix_acl_release(dacl);
+	// posix_acl_release(pacl);
+	return err;
+}
+
+static int hypervfs_create(struct inode *dir, struct dentry *dentry, umode_t omode, bool excl)
+{
+	return hypervfs_mknod(dir, dentry, omode, 0);
+}
+
+static struct dentry *hypervfs_lookup(struct inode *dir, struct dentry *entry, unsigned int flags)
+{
+	int err;
+	struct inode *inode;
+	struct dentry *newent;
+
+	if (entry->d_name.len > NAME_MAX)
+		return ERR_PTR(-ENAMETOOLONG);
+
+	// TODO: fetch the data from the server
+
+	newent = d_splice_alias(inode, entry);
+	err = PTR_ERR(newent);
+	if (IS_ERR(newent))
+		goto out_err;
+
+	return newent;
+
+ out_iput:
+	iput(inode);
+ out_err:
+	return ERR_PTR(err);
+}
+
+static int hypervfs_atomic_open(struct inode *dir, struct dentry *entry, struct file *file, unsigned flags, umode_t mode)
+{
+	int err;
+	struct dentry *res = NULL;
+
+	if (d_in_lookup(entry)) {
+		res = hypervfs_lookup(dir, entry, 0);
+		if (IS_ERR(res))
+			return PTR_ERR(res);
+
+		if (res)
+			entry = res;
+	}
+
+	if (!(flags & O_CREAT) || d_really_is_positive(entry))
+		goto out;
+
+	/* Only creates */
+	file->f_mode |= FMODE_CREATED;
+
+	err = hypervfs_mknod(dir, entry, mode, 0);
+	if (err) {
+		dput(res);
+		return err;
+	}
+
+out:
+	return finish_no_open(file, res);
+}
+
+static int hypervfs_getattr(const struct path *path, struct kstat *stat, u32 request_mask, unsigned int flags)
+{
+	struct dentry *dentry = path->dentry;
+
+	generic_fillattr(d_inode(dentry), stat);
+
+	return 0;
+
+}
+
 static const struct inode_operations hypervfs_dir_inode_operations = {
-	// .create = hypervfs_vfs_create,
-	// .atomic_open = hypervfs_vfs_atomic_open,
-	// .lookup = hypervfs_vfs_lookup,
+	.create = hypervfs_create,
+	.atomic_open = hypervfs_atomic_open,
+	.lookup = hypervfs_lookup,
 	// .link = hypervfs_vfs_link,
 	// .symlink = hypervfs_vfs_symlink,
 	// .unlink = hypervfs_vfs_unlink,
 	// .mkdir = hypervfs_vfs_mkdir,
 	// .rmdir = hypervfs_vfs_rmdir,
-	// .mknod = hypervfs_vfs_mknod,
+	.mknod = hypervfs_mknod,
 	// .rename = hypervfs_vfs_rename,
-	// .getattr = hypervfs_vfs_getattr,
+	.getattr = hypervfs_getattr,
 	// .setattr = hypervfs_vfs_setattr,
 	// .listxattr = hypervfs_listxattr,
 	// .get_acl = hypervfs_iop_get_acl,
 };
 
 static const struct inode_operations hypervfs_file_inode_operations = {
-	// .getattr = hypervfs_vfs_getattr,
+	.getattr = hypervfs_getattr,
 	// .setattr = hypervfs_vfs_setattr,
 	// .listxattr = hypervfs_listxattr,
 	// .get_acl = hypervfs_iop_get_acl,
@@ -132,7 +237,7 @@ static const struct inode_operations hypervfs_file_inode_operations = {
 
 static const struct inode_operations hypervfs_symlink_inode_operations = {
 	// .get_link = hypervfs_vfs_get_link,
-	// .getattr = hypervfs_vfs_getattr,
+	.getattr = hypervfs_getattr,
 	// .setattr = hypervfs_vfs_setattr,
 	// .listxattr = hypervfs_listxattr,
 };
